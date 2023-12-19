f8f3e96e550329259d159e40dc33722d7e90c8fd
Jeudyl Robles Pidiache <jeudyl.robles-pidiache@uni-konstanz.de>
2023-12-19 23:25:26 +0100

    primerPrime

--- 48PalindromoConStrrev.cpp
+++ 48PalindromoConStrrev.cpp
@@ -1,29 +1,44 @@
+/*// Mis Ideas PRIMEEEEEEEEEEEEEEEEEEE
 #include <iostream>
-#include <string.h>
-#include <algorithm>
-
-void strrev(char *begin, char *end)
-{
-    while (begin < end)
-    {
-        std::swap(*begin++, *end--);
-    }
-}
-
+#include <string>
+#include <cstring>
+using namespace std;
 int main()
 {
-    char texto[40], textoRev[40];
-regresa:
-    std::cout << "Humano escribe un palindromo: ";
-    std::cin >> texto;
-    strcpy(textoRev, texto);
-    strrev(textoRev, textoRev + strlen(textoRev) - 1);
-    if (strcmp(texto, textoRev) != 0)
-    {
-        std::cout << "Humano estupido esto (" << texto << " " << textoRev << ") no es un palindromo\n";
-        goto regresa;
-    }
-    std::cout << "Humano bueno, ahora dame la patita\n";
 
-    return 0;
+    string inOrder, reverse;
+regresar:
+    cout << "Dame el texto que quieres comprobar: ";
+    getline(cin, inOrder);
+
+    char orderArr[inOrder.length() + 1];
+    strcpy(orderArr, inOrder.c_str());
+
+    cout << "Este es tu texto en un orden correcto: ";
+    for (int i = 0; i < inOrder.length(); i++)
+    {
+        cout << orderArr[i];
+    }
+    cout << "\n";
+
+    char reversedArr[inOrder.length() + 1];
+    cout << "Este es tu texto al reves: ";
+    for (int i = inOrder.length() - 1, j = 0; i >= 0; i--, j++) //'i--' y 'j++' es como decir "Después de mover cada bloque, voy a pasar al bloque anterior en mi vieja fila y al siguiente espacio en mi nueva fila."
+    {
+        reversedArr[j] = orderArr[i];
+        cout << reversedArr[j];
+    }
+    cout << "\n";
+    reversedArr[inOrder.length()] = '\0'; // esta linea ayuda a la funcion 'srtrrcmp()' a saber cuando han llegado al final de la cadena
+    if (strcmp(orderArr, reversedArr) == 0)
+    {
+        cout << "El texto ES un PALINDROMO. \n";
+    }
+    else
+    {
+        cout << "El texto NO ES un PALINDROMO. \nIntentalo de nuevo!\n";
+        goto regresar;
+    }
 }
+*/
+// TUTORIAL
